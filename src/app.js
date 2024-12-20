40be9a3d54675444821dba83cea07d979d3aef69
Czer0C <trieunguyen04239@gmail.com>
2024-12-20 09:34:22 +0000

    mmmmm

--- src/app.js
+++ src/app.js
@@ -7,9 +7,15 @@ const app = express();
 // Middleware
 app.use(express.json());
 
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+
+
 // Routes
 app.use("/api", uploadRoutes);
 
+
+
 // Start server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
