c27c40333ce6cd69f6619ffc6e755947916db251
Czer0C <trieunguyen04239@gmail.com>
2024-12-20 17:15:40 +0700

    upl

--- src/app.js
+++ src/app.js
@@ -1,21 +1,18 @@
-const express = require("express");
-const uploadRoutes = require("./routes/uploadRoutes");
-require("dotenv").config();
+const express = require('express')
+const uploadRoutes = require('./routes/uploadRoutes')
+require('dotenv').config()
 
-const app = express();
+const app = express()
 
 // Middleware
-app.use(express.json());
+app.use(express.json())
 
-app.use(express.json({ limit: "100mb" }));
-app.use(express.urlencoded({ limit: "100mb", extended: true }));
-
+app.use(express.json({ limit: '100mb' }))
+app.use(express.urlencoded({ limit: '100mb', extended: true }))
 
 // Routes
-app.use("/api", uploadRoutes);
-
-
+app.use('/api', uploadRoutes)
 
 // Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
